0af38c54f508b506fd3eb43f77d6cfd35a5ea32c
TELEK1NET1C (TK1) <ashlynmae21@gmail.com>
2025-04-30 17:22:19 -0400

    Add gallery.

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,19 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using BepInEx;
 using UnityEngine;
 using SlugBase.Features;
 using static SlugBase.Features.FeatureTypes;
+using MonoMod;
+using System.Xml.Serialization;
+using IL;
+using IL.Stove.Sample.Ownership;
+using RWCustom;
+using Expedition;
 
-namespace SlugTemplate
+namespace GuidingLight
 {
     [BepInPlugin(MOD_ID, "The Guiding Light", "0.0.1")]
     class Plugin : BaseUnityPlugin
     {
         private const string MOD_ID = "tk1.guidinglight";
-
-        public static readonly PlayerFeature<float> SuperJump = PlayerFloat("guidinglight/super_jump");
-        public static readonly PlayerFeature<bool> ExplodeOnDeath = PlayerBool("guidinglight/explode_on_death");
-        public static readonly GameFeature<float> MeanLizards = GameFloat("guidinglight/mean_lizards");
+        private int startSpriteIndex;
+        private FSprite newPlayerSprite;
 
 
         // Add hooks
@@ -22,64 +27,68 @@ namespace SlugTemplate
             On.RainWorld.OnModsInit += Extras.WrapInit(LoadResources);
 
             // Put your custom hooks here!
-            On.Player.Jump += Player_Jump;
-            On.Player.Die += Player_Die;
-            On.Lizard.ctor += Lizard_ctor;
         }
-        
+
         // Load any resources, such as sprites or sounds
         private void LoadResources(RainWorld rainWorld)
         {
+
         }
-
-        // Implement MeanLizards
-        private void Lizard_ctor(On.Lizard.orig_ctor orig, Lizard self, AbstractCreature abstractCreature, World world)
+        
+        private void PlayerGraphics_InitiateSprites(On.PlayerGraphics.orig_InitiateSprites orig, PlayerGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam)
         {
-            orig(self, abstractCreature, world);
+            orig(self, sLeaser, rCam); // Call the original method's code first
 
-            if(MeanLizards.TryGet(world.game, out float meanness))
+            // Assuming you're using SlugBase, use the value of SlugCatClass to match the string ID of your slugcat
+            if (self.player.SlugCatClass.value == "tk1.guidinglight")
             {
-                self.spawnDataEvil = Mathf.Min(self.spawnDataEvil, meanness);
+                startSpriteIndex = sLeaser.sprites.Length; // IT'S HIGHLY RECCOMMENDED TO USE CONDITIONAL WEAK TABLES FOR YOUR VALUES, this is for a simple example
+
+                Array.Resize(ref sLeaser.sprites, sLeaser.sprites.Length + 1); // The array now has an extra slot at the end of it's content for your sprite
+
+                sLeaser.sprites[startSpriteIndex] = new FSprite("Belt"); // This is now your new sprite, it has no extra properties and is assumed to be a singular sprite
+                newPlayerSprite = sLeaser.sprites[startSpriteIndex]; // ADD THIS TO YOUR CWT AS WELL, assigning the sprite to a variable makes it easier to read your code
             }
         }
 
-
-        // Implement SuperJump
-        private void Player_Jump(On.Player.orig_Jump orig, Player self)
+        private void PlayerGraphics_AddToContainer(On.PlayerGraphics.orig_AddToContainer orig, PlayerGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, FContainer newContatiner)
         {
-            orig(self);
+            orig(self, sLeaser, rCam, newContatiner); // Call the original method's code first
 
-            if (SuperJump.TryGet(self, out var power))
+            // Cautional check for both if it's your slugcat AND the sLeaser.sprites array has been resized properly
+            if (self.player.SlugCatClass.value == "tk1.guidinglight" && sLeaser.sprites.Length > startSpriteIndex && newPlayerSprite != null)
             {
-                self.jumpBoost *= 1f + power;
+                newPlayerSprite.RemoveFromContainer(); // Cautionary removal of the sprite in case it was not properly removed automatically
+                rCam.ReturnFContainer("Midground"); // Change the name to the appropriate container
             }
         }
 
-        // Implement ExlodeOnDeath
-        private void Player_Die(On.Player.orig_Die orig, Player self)
+        private void PlayerGraphics_DrawSprites(On.PlayerGraphics.orig_DrawSprites orig, PlayerGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
         {
-            bool wasDead = self.dead;
+            orig(self, sLeaser, rCam, timeStacker, camPos);  // Call the original method's code first
 
-            orig(self);
-
-            if(!wasDead && self.dead
-                && ExplodeOnDeath.TryGet(self, out bool explode)
-                && explode)
+            // Same check as the previous hook
+            if (self.player.SlugCatClass.value == "tk1.guidinglight" && sLeaser.sprites.Length > startSpriteIndex && newPlayerSprite != null)
             {
-                // Adapted from ScavengerBomb.Explode
-                var room = self.room;
-                var pos = self.mainBodyChunk.pos;
-                var color = self.ShortCutColor();
-                room.AddObject(new Explosion(room, self, pos, 7, 250f, 6.2f, 2f, 280f, 0.25f, self, 0.7f, 160f, 1f));
-                room.AddObject(new Explosion.ExplosionLight(pos, 280f, 1f, 7, color));
-                room.AddObject(new Explosion.ExplosionLight(pos, 230f, 1f, 3, new Color(1f, 1f, 1f)));
-                room.AddObject(new ExplosionSpikes(room, pos, 14, 30f, 9f, 7f, 170f, color));
-                room.AddObject(new ShockWave(pos, 330f, 0.045f, 5, false));
-
-                room.ScreenMovement(pos, default, 1.3f);
-                room.PlaySound(SoundID.Bomb_Explode, pos);
-                room.InGameNoise(new Noise.InGameNoise(pos, 9000f, self, 1f));
+                newPlayerSprite.SetPosition(sLeaser.sprites[0].GetPosition()); // Sets the position of the new sprite to that of the head sprite
+                /* FSprites contain a lot of properties to mess with, and messing with relative positions can be found in various DrawSprite methods of different creatures
+                 * RWCustom contains many different Rain World built methods to handle various mathmatical equations for you, such as Custom.AimBetweenTwoVectors() which can find the rotation angle between two positions
+                 * timeStacker can be used for sin, cos, or tan related timers or values
+                 * camPos refers to the camera position, and is used for referring to the visual offset a body part actually has with the geometry of the camera
+                 * the PlayerGraphics instance also has loads of access to various player-based things
+                 * */
             }
         }
+
+        private void PlayerGraphics_ApplyPalette(On.PlayerGraphics.orig_ApplyPalette orig, PlayerGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, RoomPalette palette)
+        {
+            orig(self, sLeaser, rCam, palette);  // Call the original method's code first
+
+            if (self.player.SlugCatClass.value == "tk1.guidinglight" && sLeaser.sprites.Length > startSpriteIndex && newPlayerSprite != null)
+            {
+                newPlayerSprite.color = new(255, 255, 255); // Setting your color here also gives you the advantage to grab the original colors of a sprite before they're changed by the DrawSprites method if they are changed at all
+            }
+        }
+
     }
 }
